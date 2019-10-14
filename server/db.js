9a2d873745606dde2c72d023f14cd6b658746b1a
always-awake <leemirim9654@gmail.com>
2019-10-14 23:15:18 +0900

    [ ENV ] mysql과 Sequelize 연동 (#2)
    why
    - Sequelize(orm)를 이용해 mysql db를 조작하기 위함
    
    what
    - Sequelize 인스턴스를 생성하여 연결
    - connection pool 설정 매개변수 추가
    - .authenticate 함수로 db 연결 확인

---
+++ server/db.js
@@ -0,0 +1,34 @@
+const Sequelize = require('sequelize');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    },
+    define: {
+      freezeTableName: true,
+    },
+  },
+);
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('✅  Connection has been established successfully.');
+  })
+  .catch((err) => {
+    console.error('❌  Unable to connect to the database:', err);
+  });
+
+module.exports = sequelize;
