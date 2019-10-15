7dcbcd7c17aab7c213c0255032854f6aafa89c3b
always-awake <leemirim9654@gmail.com>
2019-10-15 21:31:55 +0900

    [ UTIL ] user 객체와 지속시간(문자열)을 매개변수로 받아 jwt 토큰을 생성해주는 유틸함수 구현
    why
    - 호출하는 위치와 상관없이 user 객체와 지속시간으로 생성된 jwt 토큰을 리턴받기 위함
    
    what
    - jsonwebtoken 모듈과 jwtConfig 파일을 이용해 jwt 토큰 생성
    - 매개 변수로 받은 authUser에서 jwt토큰에 암호화하여 포함시킬 정보만 선별
    - 매개 변수로 받은 duration 값으로 토큰 만료 시간 설정

---
+++ server/utils/jwt.js
@@ -0,0 +1,26 @@
+const jwt = require('jsonwebtoken');
+const jwtConfig = require('../config/jwt');
+
+/**
+ * 인증된 유저와 jwt 토큰의 지속시간을 매개변수로 받아 jwt 토큰을 리턴하는 유틸 함수
+ * @param {user} authUser
+ * @param {string} duration
+ */
+const createJwtToken = (authUser, duration) => {
+  const jwtToken = jwt.sign(
+    {
+      pk: authUser.pk,
+      id: authUser.id,
+      name: authUser.name,
+    },
+    jwtConfig.secretKey,
+    {
+      expiresIn: duration,
+    },
+  );
+  return jwtToken;
+};
+
+module.exports = {
+  createJwtToken,
+};
