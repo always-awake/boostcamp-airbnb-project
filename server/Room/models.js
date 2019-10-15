9b24f82ef145155a4f962838888b49abb864cf83
always-awake <leemirim9654@gmail.com>
2019-10-15 15:44:16 +0900

    [ IMPROVE ] 숙소(room) 모델 구현 (#4)
    why
    - 숙소에 대한 정보를 저장할 필요성이 있음
    
    what
    - Room 데이터 저장에 필요한 pk, name, type, price, bio 등의 필드를 갖는 모델 생성
    - 모델 코드를 구현할 때, orm(시퀄라이즈)을 사용

---
+++ server/Room/models.js
@@ -0,0 +1,93 @@
+const Sequelize = require('sequelize');
+const sequelize = require('../db');
+const user = require('../User/models');
+
+const room = sequelize.define('room', {
+  pk: {
+    type: Sequelize.BIGINT,
+    autoIncrement: true,
+    primaryKey: true,
+    allowNull: false,
+  },
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  bio: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  mainPhoto: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  type: {
+    type: Sequelize.ENUM(
+      'Whole house',
+      'Private room',
+      'Hotel room',
+      'Shared room',
+    ),
+    allowNull: false,
+  },
+  price: {
+    type: Sequelize.BIGINT,
+    allowNull: false,
+  },
+  bedCount: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+  },
+  bedroomCount: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+  },
+  bathroomCount: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+  },
+  isSuperHost: {
+    type: Sequelize.BOOLEAN,
+    allowNull: false,
+    defaultValue: false,
+  },
+  headCount: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    defaultValue: 0,
+  },
+  adultCount: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    defaultValue: 0,
+  },
+  childCount: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    defaultValue: 0,
+  },
+  babyCount: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    defaultValue: 0,
+  },
+  facilities: {
+    type: Sequelize.STRING,
+  },
+}, {
+  charset: 'utf8',
+  tableName: 'rooms',
+});
+
+user.hasMany(
+  room, {
+    as: 'rooms',
+    foreignKey: {
+      name: 'hostPk',
+      allowNull: false,
+    },
+    sourceKey: 'pk',
+  },
+);
+
+module.exports = room;
