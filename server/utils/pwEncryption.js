f11e3457d718e8ab39778e7d72fada4c52b520d6
always-awake <leemirim9654@gmail.com>
2019-10-16 14:44:55 +0900

    [ UTIL ] 유저가 입력한 비밀번호를 uuid 값으로 생성된 salt를 사용하여 암호화된 비밀번호를 생성하는 유틸함수 구현
    why
    - 유저 비밀번호를 db에 저장할 때 암호하여, 유저 데이터 보안을 높이기 위함
    
    what
    - 내장 모듈 crypto를 사용하여 유저가 입력한 비밀번호를 암호화 함
    - 암호화 할 때 임의의 랜덤 문자열 salt 값을 첨가하여 암호화된 비밀번호를 생성함
    - salt값을 생성할 때, uuid/v4 모듈을 사용함

---
+++ server/utils/pwEncryption.js
@@ -0,0 +1,14 @@
+const crypto = require('crypto');
+const uuidv4 = require('uuid/v4');
+
+const createHashPassword = (userPw) => {
+  const newUuidSalt = uuidv4();
+  return {
+    hashPassword: crypto.createHash('sha512').update(userPw + newUuidSalt).digest('base64'),
+    uuidSalt: newUuidSalt,
+  };
+};
+
+module.exports = {
+  createHashPassword,
+};
