7e8c3322624f808f39ba05268fb640bdd8de1f03
always-awake <leemirim9654@gmail.com>
2019-10-17 01:48:14 +0900

    [ FIX ] 로그인 요청을 처리하는 함수에 암호화된 pw일치 여부를 판별하여 결과에 따라 다른 응답을 하도록 수정
    why
    - 입력받은 비밀번호를 암호화하여 db에 저장된 비밀번호와 일치 여부에 따라 클라이언트에게 다른 응답을 해야한다고 생각
    
    what
    - checkPwValidation 유틸 함수를 사용하여 유효한 비밀번호인 것을 확인
    - 유효한 비밀번호라면, jwt 토큰을 생성하여 쿠키에 넣고 200 응답
    - 아니라면, 401 응답과 상황에 맞는 msg 응답

--- server/User/controllers.js
+++ server/User/controllers.js
@@ -1,36 +1,46 @@
 const userModel = require('./models');
 const { createJwtToken } = require('../utils/jwt');
-const { createHashPassword } = require('../utils/pwEncryption');
+const {
+  createHashPassword,
+  checkPwValidation,
+} = require('../utils/pwEncryption');
 
 /**
  * 유저 로그인 요청을 처리하는 컨트롤러 함수
- * 유효한 유저라면, jwt 토큰을 생성하여 응답
+ * 유효한 유저라면, jwt 토큰을 생성하여 쿠키 값에 삽입 후, 응답
  * @param {*} req
  * @param {*} res
  */
 const login = async (req, res) => {
   const { userId, userPw } = req.body;
-  const authUser = await userModel.findOne({
+  const user = await userModel.findOne({
     where: {
-      id: userId,
-      password: userPw,
+      accountId: userId,
     },
   }) || false;
 
-  if (authUser) {
-    res.status(200);
-    res.cookie(
-      'jwtToken',
-      createJwtToken(authUser, '5m'),
-      { httpOnly: true, signed: true },
-    );
-    res.json({
-      msg: 'login success!',
-    });
+  if (user) {
+    const { accountPw, salt } = user.dataValues;
+    if (checkPwValidation(userPw, accountPw, salt)) {
+      res.status(200);
+      res.cookie(
+        'jwtToken',
+        createJwtToken(user, '5m'),
+        { httpOnly: true, signed: true },
+      );
+      res.json({
+        msg: 'login success!',
+      });
+    } else {
+      res.status(401);
+      res.json({
+        msg: 'worng Pw! try again!',
+      });
+    }
   } else {
     res.status(401);
     res.json({
-      msg: 'login fail! try again!',
+      msg: 'worng Id! try again!',
     });
   }
 };
@@ -38,6 +48,7 @@ const login = async (req, res) => {
 /**
  * 회원가입 요청을 처리하는 컨트롤러 함수
  * req 인자 내 비밀번호 값을 암호화하여 저장함
+ * jwt 토큰을 생성하여 쿠키 값에 삽입 후, 응답
  * @param {*} req
  * @param {*} res
  */
