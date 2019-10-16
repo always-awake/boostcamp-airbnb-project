27d1e13c74097b4f46a09820dfdb34a51d8704c4
always-awake <leemirim9654@gmail.com>
2019-10-17 00:29:32 +0900

    [ IMPROVE ] cookie-parser 모듈 설치 및 초기 설정
    why
    - 클라이언트에게 jwt 토큰을 넣을 쿠키 발급을 위해
    
    what
    - yarn을 이용해 cookie-parser 모듈 설치
    - 서명된 쿠키를 발급해주기 위해 쿠키의 secret key 설정
    - app이 cookie-parser 모듈을 사용하도록 초기화

--- server/app.js
+++ server/app.js
@@ -1,12 +1,17 @@
 const path = require('path');
+const cookieParser = require('cookie-parser');
 const express = require('express');
+const dotenv = require('dotenv');
 const routers = require('./config/routers');
+
+dotenv.config();
 
 const app = express();
 
 app.use('/assets', express.static(path.resolve(__dirname, 'assets')));
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser(process.env.COOKIE_SECRET_KEY));
 app.use(routers);
 
 module.exports = app;
