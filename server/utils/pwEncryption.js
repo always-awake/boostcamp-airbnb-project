6bdcad37bad5d3a737d09f87753ef157bbcb0cd0
always-awake <leemirim9654@gmail.com>
2019-10-16 22:16:04 +0900

    [ CHORE ] createHashPassword 함수내 코드 배치를 수정함
    why
    - 기존의 긴 코드보다 가독성이 향상된다고 생각하기 때문
    
    what
    - 한줄에 하나의 함수만 표시되도록 코드를 수정함

--- server/utils/pwEncryption.js
+++ server/utils/pwEncryption.js
@@ -4,7 +4,10 @@ const uuidv4 = require('uuid/v4');
 const createHashPassword = (userPw) => {
   const newUuidSalt = uuidv4();
   return {
-    hashPassword: crypto.createHash('sha512').update(userPw + newUuidSalt).digest('base64'),
+    hashPassword: crypto
+      .createHash('sha512')
+      .update(userPw + newUuidSalt)
+      .digest('base64'),
     uuidSalt: newUuidSalt,
   };
 };
