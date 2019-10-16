dedfadb0079d0d9a115f43b572b78a4694af6c3e
always-awake <leemirim9654@gmail.com>
2019-10-16 22:44:02 +0900

    [ CHORE ] batch 작업 코드 내 함수와 createHashPassword 함수에 주석을 추가해줌
    why
    - 구현한 모든 함수에 대한 상세한 설명은 필수적이라고 생각하기 때문
    
    what
    - jsdoc 주석 형식으로 주석을 추가함

--- server/utils/batchJob/rooms.js
+++ server/utils/batchJob/rooms.js
@@ -3,11 +3,19 @@ const roomModel = require('../../Room/models');
 
 const roomCsvFilePath = './utils/batch/csv/room.csv';
 
+/**
+ * room.csv 파일을 읽어와 csv 데이터를 json으로 변환하는 함수
+ * 변환된 json데이터 리스트를 리턴함
+ * @returns { jsonArray }
+ */
 const getRoomDataFromCsv = async () => {
   const roomJsonArray = await csv().fromFile(roomCsvFilePath);
   return roomJsonArray;
 };
 
+/**
+ * json 데이터 리스트를 Sequelize api인 bulkCreate를 이용해 room db에 일괄적으로 insert 해주는 함수
+ */
 const updateRoomData = async () => {
   const roomJsonArray = await getRoomDataFromCsv();
   await roomModel.bulkCreate(roomJsonArray);
--- server/utils/batchJob/users.js
+++ server/utils/batchJob/users.js
@@ -3,11 +3,19 @@ const userModel = require('../../User/models');
 
 const userCsvFilePath = './utils/batch/csv/user.csv';
 
+/**
+ * user.csv 파일을 읽어와 csv 데이터를 json으로 변환하는 함수
+ * 변환된 json데이터 리스트를 리턴함
+ * @returns { jsonArray }
+ */
 const getUserDataFromCsv = async () => {
   const userJsonArray = await csv().fromFile(userCsvFilePath);
   return userJsonArray;
 };
 
+/**
+ * json 데이터 리스트를 Sequelize api인 bulkCreate를 이용해 user db에 일괄적으로 insert 해주는 함수
+ */
 const updateUserData = async () => {
   const userData = await getUserDataFromCsv();
   await userModel.bulkCreate(userData);
