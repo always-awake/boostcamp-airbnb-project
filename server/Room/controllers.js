a4f03fe0e865d729b172007be5cedd7ec4c0efe3
always-awake <leemirim9654@gmail.com>
2019-10-17 23:01:49 +0900

    [ FEATURE ] 필터링 옵션에 날짜가 포함되어 있지 않을 경우 숙소 필터링 요청을 처리하는 함수 구현
    why
    - 쿼리 스트링으로 주어진 옵션에 따라 해당 옵션에 맞는 숙소를 조회하고 응답하는 api 필요
    
    what
    - addPriceOption 함수를 이용하여 filterOption 값에 포함되어 있지 않았던 가격에 대한 필터링 옵션을 추가함
    - 가격 필터링 옵션까지 추가된 filterOption 값으로 숙소 db를 조회함
    - 조회 결과를 적절한 json구조에 담아 msg와 함께 200 응답
    - 에러 발생 시, 500 응답 및 오류 메시지 전달

--- server/Room/controllers.js
+++ server/Room/controllers.js
@@ -1,5 +1,31 @@
 const roomModel = require('./models');
-const { addPriceoption } = require('../utils/filtering');
+const { addPriceOption } = require('../utils/filtering');
+
+/**
+ * 필터링 옵션으로 날짜가 포함되어 있지 않을 경우 숙소 필터링 요청을 처리하는 함수
+ * @param {*} req
+ * @param {*} res
+ */
+const filterWithoutDate = async (req, res) => {
+  try {
+    const roomList = await roomModel.findAll({
+      where: addPriceOption(req),
+    });
+    res.status(200);
+    res.json({
+      msg: 'Filtering Success!',
+      data: {
+        dataCount: roomList.length,
+        rooms: roomList,
+      },
+    });
+  } catch (e) {
+    res.status(500);
+    res.json({
+      msg: 'Sorry Try again!',
+    });
+  }
+};
 
 /**
  * 필터링 옵션에 날짜 존재 여부에 따라 다른 함수를 호출하는 함수
