30f37d98a5674673be59338cde7f291647a67321
always-awake <leemirim9654@gmail.com>
2019-10-20 17:03:58 +0900

    [ FEATURE ] 필터링 옵션에 날짜가 포함되어 있을 경우 숙소 필터링 요청을 처리하는 함수 구현
    Why
    - 쿼리 스트링으로 주어진 옵션에 따라 해당 옵션에 맞는 숙소를 조회하는 api 필요
    
    What
    - 쿼리 스트링 내 checkIn, checkOut 값으로 예약 테이블을 조회하여 checkIn~checkOut 기간과 겹치는 예약 목록 조회
    - filterOption 내 필터링 조건에 조회한 예약 목록 내 roomId를 갖고 있지 않은 숙소만 조회할 수 있도록 설정 추가
    - 필터링이 성공적으로 이루어졌다면, 200 응답
    - 아니라면, 500 응답

--- server/Room/controllers.js
+++ server/Room/controllers.js
@@ -1,5 +1,40 @@
 const roomModel = require('./models');
-const { addPriceOption } = require('../utils/filtering');
+const reservationModel = require('../Reservation/models');
+const {
+  addPriceOption,
+  checkReservationOption,
+  refineReservations,
+  addDatePriceOption,
+} = require('../utils/filtering');
+
+/**
+ * 필터링 옵션으로 날짜가 포함되어 있을 경우 숙소 필터링 요청을 처리하는 함수
+ * @param {*} req
+ * @param {*} res
+ */
+const filterWithDate = async (req, res) => {
+  try {
+    const overlapedReservations = await reservationModel.findAll({
+      where: checkReservationOption(req.query),
+      attributes: ['roomId'],
+    });
+    const reservedRoomIdList = refineReservations(overlapedReservations);
+    const roomList = await roomModel.findAll({
+      where: addDatePriceOption(req, reservedRoomIdList),
+    });
+    res.status(200).json({
+      msg: 'Filtering Success!',
+      data: {
+        dataCount: roomList.length,
+        rooms: roomList,
+      },
+    });
+  } catch (e) {
+    res.status(500).json({
+      msg: 'Sorry Try again!',
+    });
+  }
+};
 
 /**
  * 필터링 옵션으로 날짜가 포함되어 있지 않을 경우 숙소 필터링 요청을 처리하는 함수
