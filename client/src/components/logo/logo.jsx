aa3925ff139b3fe8ef6961d25659699eeb20d516
always-awake <leemirim9654@gmail.com>
2019-10-22 13:29:22 +0900

    [ FEATURE ] 헤더 내 로고 컴포넌트 만들기 (#19)
    Why
    - 헤더 내 사용될 로고 컴포넌트가 필요함
    
    What
    - 로고 이미지가 포함된 로고 컴포넌트를 구현함
    - 함수형 컴포넌트로 구현함

---
+++ client/src/components/logo/logo.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const LOGO_IMG_PATH = '../../../assets/img/airbnb-logo.png';
+const LOGO_IMG_ALT = 'airbnb-logo';
+
+const Logo = styled.div`
+height: 35px;
+weight: 35px;
+margin: 0 25px 0 25px;
+`;
+const LogoImg = styled.img`
+height: 100%;
+weight: 100%;
+`;
+
+const logo = () => {
+  return (
+    <Logo>
+      <LogoImg src={LOGO_IMG_PATH} alt={LOGO_IMG_ALT} />
+    </Logo>
+  );
+};
+
+export default logo;
