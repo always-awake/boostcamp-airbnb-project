069bfb79eb4803c367e6ad9cceec1174caf7a464
always-awake <leemirim9654@gmail.com>
2019-10-23 21:19:24 +0900

    [ FEATURE ] 숙소 컴포넌트 목록을 보여주는 RoomList 컴포넌트 구현 (#28)
    Why
    - 숙소 컴포넌트 리스트를 보여주는 컴포넌트 필요
    
    What
    - props 내 rooms 데이터 배열 내 데이터 수 만큼 Room 컴포넌트를 생성
    - 스타일 컴포넌트로 스타일 지정
    - Room 컴포넌트에 적절한 props 전달

--- client/src/containers/searchView/roomList/roomList.jsx
+++ client/src/containers/searchView/roomList/roomList.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import * as S from './styles.jsx';
 import Room from '../../../components/room/room.jsx';
 
 const RoomList = (props) => {
-  const { rooms } = props || false;
+  const { rooms } = props;
   const createRooms = () => {
     return rooms.map((room) => {
       return (
@@ -19,7 +20,7 @@ const RoomList = (props) => {
           hostId={room.hostId}
           headCount={room.headCount}
           bedCount={room.bedCount}
-          bedRoomCount={room.bedRoomCount}
+          bedroomCount={room.bedroomCount}
           bathroomCount={room.bathroomCount}
         />
       );
@@ -28,9 +29,17 @@ const RoomList = (props) => {
 
   return (
     <S.RoomList>
+      <S.RoomListHeader>
+        <S.RoomListHeaderImg src="https://a0.muscache.com/airbnb/static/packages/icon-uc-trophy.9ee78aa1.gif" />
+        숙소에 대해  1,000,000개 이상의 게스트 후기가 있으며, 평점은 5점 만점입니다.
+      </S.RoomListHeader>
       {rooms && createRooms()}
     </S.RoomList>
   );
+};
+
+RoomList.propTypes = {
+  rooms: PropTypes.array.isRequired,
 };
 
 export default RoomList;
