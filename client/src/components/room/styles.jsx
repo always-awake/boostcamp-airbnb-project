647efe7da230bd889f6d528aa1d28a4feb45b68d
always-awake <leemirim9654@gmail.com>
2019-10-23 20:34:59 +0900

    [ FEATURE ] 숙소 정보를 보여주는 Room 컴포넌트 구현 (#27)
    Why
    - 숙소 정보를 보여줄 컴포넌트 필요
    
    What
    - props로 받은 숙소에 대한 정보를 적절한 하위 컴포넌트를 이용해 렌더링함
    - Room 함수 컴포넌트 구현

--- client/src/components/room/styles.jsx
+++ client/src/components/room/styles.jsx
@@ -1,30 +1,15 @@
 /* eslint-disable import/prefer-default-export */
 import styled from 'styled-components';
 
-export const ReservationButton = styled.div`
-  display: flex;
-  flex-display: row;
-  align-items: center;
-  justify-content: center;
-  padding: 0 15px 0 15px;
-  margin: 0 5px 0 5px;
-  width: 80px;
-  height: 36px;
-  border-radius: 30px;
-  color: #FFFFFF;
-  background-color: #EB5B62;
-`;
-
 export const Room = styled.div`
   display: flex;
-  flex-display: row;
+  flex-direction: row;
   align-items: center;
-  justify-content: center;
-  width: 1000px;
-  height: 250px;
+  justify-content: space-between;
+  width: 950px;
+  height: 260px;
   border: 1px solid #EBEBEB;
   color: #222222;
-`;
-
-export const RoomInfo = styled.div`
+  padding: 0 30px 0 30px;
+  margin-top: 30px;
 `;
