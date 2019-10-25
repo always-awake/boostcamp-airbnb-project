0fbaf0f2f4b5f7927c1867dcd9a1e38aaa04963a
always-awake <leemirim9654@gmail.com>
2019-10-25 11:27:21 +0900

    [ FEATURE ] 서버에서 일부 도메인(같은 도메인이 아닌 도메인)에서의 요청만 처리할 수 있도록 설정
    Why
    - 클라이언트 코드가 실행되고 있는 서버 도메인 주소에서 발생하는 요청만 처리하기 위함
    
    Whay
    - cors 모듈 설치
    - 특정 url 주소에서 발생하는 요청만 처리할 수 있도록 app.js에 설정(whitelist 배열 내 있는 주소만 처리)

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
 const express = require('express');
 const dotenv = require('dotenv');
 const routers = require('./config/routers');
@@ -7,6 +8,20 @@ const routers = require('./config/routers');
 dotenv.config();
 
 const app = express();
+
+// cors setting
+const whitelist = ['http://localhost:4000'];
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+};
+app.use(cors(corsOptions));
 
 app.use('/assets', express.static(path.resolve(__dirname, 'assets')));
 app.use(express.json());
