4d949d2c7f733ac08ff28be224b5224f2c60886a
always-awake <leemirim9654@gmail.com>
2019-10-25 10:44:30 +0900

    [ FEATURE ] 가격 설정 모달창 구현 (#32)
    Why
    - 가격 필터링 버튼을 클릭하면 가격 설정값을 입력할 수 있는 컴포넌트 필요
    
    What
    - PriceModal 컴포넌트 구현
    - PriceSlider 외부 모듈 이용
    - 스타일 컴포넌트로 스타일 구현

--- client/src/components/modal/styles.jsx
+++ client/src/components/modal/styles.jsx
@@ -26,3 +26,29 @@ export const CountSetting = styled.div`
   align-items: center;
   justify-content: space-between;
 `;
+
+export const PriceModal = styled.div`
+  width: 380px;
+  height: 210px;
+  border: 1px solid #D9D9D9;
+  border-radius: 10px;
+  position: absolute;
+  background-color: #FFFFFF;
+  box-shadow: rgba(0, 0, 0, 0.15) 0px 10px 37px;
+  margin: 10px 0 0 248px;
+`;
+
+export const PriceSliderTitle = styled.div`
+  display: flex;
+  flex-direction: column;
+  font-size: 16px;
+  color: #222222;
+  width: 100%;
+  align-items: center;
+  margin-top: 20px;
+`;
+
+export const PriceSliderSubTitle = styled.div`
+  font-size: 12px;
+  color: #717171;
+`;
