1c7006d255701fd4e36f86df7b087bbacef02260
always-awake <leemirim9654@gmail.com>
2019-10-25 10:30:31 +0900

    [ FIX ] urlQuery 매개변수 추가
    Why
    - 숙소 필터링 옵션을 적용할 수 있는 컴포넌트가 개발됨에 따라 query 옵션이 추가된 url로 fetch 요청을 하기 위함
    
    What
    - urlQuery 변경이 될 경우에만 fetch 요청을 보낼 수 있도록 dependencies array 설정

--- client/src/utils/hooks/useGet.js
+++ client/src/utils/hooks/useGet.js
@@ -1,14 +1,17 @@
 import { useState, useEffect } from 'react';
+import { refineurl } from '../refine.js';
 
 /**
- * 매개변수로 받은 url 주소로 fetch 요청 후, 응답 받은 데이터를 리턴하는 커스텀 Hook
+ * 매개변수로 받은 url, urlQuery를 조합한 refindeUrl로 fetch 요청
+ * refinedUrl 주소로 fetch 요청 후, 응답 받은 데이터를 리턴하는 커스텀 Hook
  * @param { String } url
  * @returns { Array } data array
  */
-const useGet = (url) => {
+const useGet = (url, urlQuery) => {
+  const refinedUrl = refineurl(url, urlQuery);
   const [data, setData] = useState({});
   const fetchUrl = async () => {
-    const response = await fetch(url, {
+    const response = await fetch(refinedUrl, {
       method: 'GET',
       credentials: 'include',
     });
@@ -18,7 +21,7 @@ const useGet = (url) => {
 
   useEffect(() => {
     fetchUrl();
-  }, []);
+  }, [urlQuery]);
 
   return [data];
 };
